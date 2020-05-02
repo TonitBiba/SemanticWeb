920a77b46029836225c5630959819ec1be13016d
TonitBiba <tonit.biba@hotmail.com>
2020-05-02 18:25:22 +0200

    Improvoments.

--- ApplicationUW/ApplicationUW/Controllers/HomeController.cs
+++ ApplicationUW/ApplicationUW/Controllers/HomeController.cs
@@ -1,27 +1,16 @@
 ﻿using ApplicationUW.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Linq;
 using VDS.RDF;
-using VDS.RDF.Parsing;
 using VDS.RDF.Query;
-using VDS.RDF.Query.Builder;
 
 namespace ApplicationUW.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
@@ -29,15 +18,13 @@ namespace ApplicationUW.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(QueryOntology queryOntology)
+        public PartialViewResult _Index(QueryOntology queryOntology)
         {
             IGraph g = new Graph();
 
             StreamReader file = new StreamReader("Config.json");
             JsonSerializer serializer = new JsonSerializer();
             var configurations = JsonConvert.DeserializeObject<Configuration>(file.ReadToEnd());
-
-
 
             if (queryOntology.Ontology == 1)
                 g.LoadFromFile(configurations.InstagramPath);
@@ -51,17 +38,25 @@ namespace ApplicationUW.Controllers
                 if (results is SparqlResultSet)
                 {
                     SparqlResultSet rset = (SparqlResultSet)results;
-                    foreach (SparqlResult r in rset)
+
+                    var result = rset.Select(s => new ResultQueryList
                     {
-                    }
+                        Object = s.ToString(),
+                        Predicate = s.ToString(),
+                        Subject = s.ToString()
+                    });
+                    return PartialView(result);
+
                 }
                 else if (results is IGraph)
                 {
                     IGraph resGraph = (IGraph)results;
-                    foreach (Triple t in resGraph.Triples)
-                    {
-                        //Do whatever you want with each Triple
-                    }
+                    var result = resGraph.Triples.Select(s => new ResultQueryList {
+                         Object = s.Object.ToString(),
+                         Predicate = s.Predicate.ToString(),
+                         Subject= s.Subject.ToString()
+                    });
+                    return PartialView(result);
                 }
                 else
                 {
@@ -72,12 +67,7 @@ namespace ApplicationUW.Controllers
             {
                 Console.WriteLine(queryEx.Message);
             }
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return PartialView();
         }
 
         [HttpGet]
@@ -111,10 +101,5 @@ namespace ApplicationUW.Controllers
             }
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ ApplicationUW/ApplicationUW/Models/ResultQueryList.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationUW.Models
+{
+    public class ResultQueryList
+    {
+        public string Subject { get; set; }
+
+        public string Predicate { get; set; }
+
+        public string Object { get; set; }
+
+    }
+}
